2ebf26a888c2ae9d0309870014c915539ba31ab9
mechairoi <ttsujikawa@gmail.com>
2014-12-14 19:04:43 +0900

    refine

--- examples/Test.purs
+++ examples/Test.purs
@@ -8,9 +8,8 @@ import Control.Monad.Eff.Ref
 import Data.Maybe
 import Data.Function
 import Prelude
-import DOM
 
-main :: forall e. Eff (dom :: DOM, ref :: Ref | e) Unit
+main :: forall e. Eff (dom :: V.DOM, ref :: Ref | e) Unit
 main = do
   refSt <- initState
   (State st) <- readRef refSt
@@ -24,19 +23,19 @@ foreign import documentBodyAppendChild " \
   \       document.body.appendChild(node); \
   \     }; \
   \   }; \
-  \ } " :: forall e. DOM.Node -> Eff (dom :: DOM | e) Unit
+  \ } " :: forall e. V.DOMNode -> Eff (dom :: V.DOM | e) Unit
 
 initState :: forall e. Eff (ref :: Ref | e) (RefVal State)
 initState = do
   let tree = (node "div" ([] :: [Attr]) [] Nothing Nothing) :: VTree
-  let node = createElement tree :: DOM.Node
+  let node = V.createElement tree :: V.DOMNode
   newRef $ State { app: 0, tree: tree, node: node }
 
 type AppState = Number
 
 newtype State = State { app :: AppState
                       , tree :: VTree
-                      , node :: DOM.Node
+                      , node :: V.DOMNode
                       }
 
 render :: RefVal State -> AppState -> VTree
@@ -48,37 +47,16 @@ render refSt app = node "div" ([] :: [Attr])
       Nothing Nothing
   ] Nothing Nothing
 
-increment :: forall e. RefVal State -> Eff (dom :: DOM, ref :: Ref | e) Unit
+increment :: forall e. RefVal State -> Eff (dom :: V.DOM, ref :: Ref | e) Unit
 increment refSt = do
   (State st) <- readRef refSt
   update refSt (st.app + 1)
 
-update :: forall e. RefVal State -> AppState -> Eff (dom :: DOM, ref :: Ref | e) Unit
+update :: forall e. RefVal State -> AppState -> Eff (dom :: V.DOM, ref :: Ref | e) Unit
 update refSt app = do
   let newTree = render refSt app
   (State st) <- readRef refSt
-  let patches = diff st.tree newTree
-  patch st.node patches
+  let patchObj = V.diff st.tree newTree
+  V.patch patchObj st.node
   writeRef refSt $ State st { app = app, tree = newTree }
   return unit
-
-foreign import data Patches :: *
-
-foreign import patchImpl " \
-  \ function patchImpl(node, patches) { \
-  \   return function() { require('virtual-dom/patch')(node, patches); } \
-  \ } " :: forall e. Fn2 DOM.Node Patches (Eff (dom :: DOM | e) Unit)
-
-patch :: forall e. DOM.Node -> Patches -> Eff (dom :: DOM | e) Unit
-patch = runFn2 patchImpl
-
-foreign import diffImpl
-  "var diffImpl = require('virtual-dom/diff');"
-  :: Fn2 VTree VTree Patches
-
-diff :: VTree -> VTree -> Patches
-diff = runFn2 diffImpl
-
-foreign import createElement
-  "var createElement = require('virtual-dom/create-element');"
-  :: VTree -> DOM.Node
